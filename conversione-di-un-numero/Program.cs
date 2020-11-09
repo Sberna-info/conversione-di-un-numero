cfe75d8b4407ec9e5c548855fc776563e7672fc0
andry <andry@DESKTOP-SENM7HG>
2020-11-09 08:58:30 +0100

    Add project files.

---
+++ conversione-di-un-numero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversione_di_un_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int base2, numero;
+
+            Console.Write("iserisci base in cui convertire");
+            base2 = int.Parse(Console.ReadLine());
+            Console.Write("inserisci in numero da convertire:");
+            numero = int.Parse(Console.ReadLine());
+            string risultato = Convert.ToString(numero, base2);
+            Console.WriteLine($"il numero convertito è {risultato}");
+        }
+    }
+}
